41bdb4bf78dc3e1f0551495b6a486ad11f02cb74
Juri <juri.strumpflohner@gmail.com>
2020-09-05 13:51:56 +0200

    fix storybook e2e test

--- apps/store-ui-shared-e2e/src/integration/header/header.spec.ts
+++ apps/store-ui-shared-e2e/src/integration/header/header.spec.ts
@@ -1,7 +1,9 @@
 describe('store-ui-shared: Header component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=header--primary&knob-title='));
-    
-    it('should render the component', () => {
-      cy.get('h1').should('contain', 'Welcome to store-ui-shared!');
-    });
+  beforeEach(() =>
+    cy.visit('/iframe.html?id=header--primary&knob-title=Game Store')
+  );
+
+  it('should render the component', () => {
+    cy.get('.MuiTypography-root').should('contain', 'Game Store');
+  });
 });
